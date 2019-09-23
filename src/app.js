e6bba323bc2b3bd41736d2d0031d69beed7b50b8
jesus zepeda <jesuszepeda@hotmail.com>
2019-09-23 09:48:54 -0500

    Update src/app.js

--- src/app.js
+++ src/app.js
@@ -85,7 +85,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Jes Z',
         errorMessage: 'Help article not found.'
     })
 })
@@ -93,7 +93,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Jes Z',
         errorMessage: 'Page not found.'
     })
 })
